adb713cc1130f3456bc4306bd64e4432df617389
Bomani McClendon <bomanimcclendon2017@u.northwestern.edu>
2017-03-28 14:52:13 -0500

    Adds all input strings and a basic layout of the input page (unstyled)

--- imports/strings/strings.js
+++ imports/strings/strings.js
@@ -1,5 +1,65 @@
 export default {
-	"SNAPSHOT_MAIN_TILE"              : "Snapshot",
-	"SNAPSHOT_MAIN_INSTRUCTIONS_BASE" : "Please answer the sections on the left, the sections on the right will give you instructions as well as best practices to answer each section",
-	"SNAPSHOT_MAIN_INSTRUCTIONS_TIMING" : "This should take a total of 15 minutes to complete – please do not spend anymore time on it. For the four sections you should spend about 2 to 3 minutes on each section and then review at the end for the remaining time to improve any answers. This a recommendation - just make sure to complete it in 15 minutes.",
+	"Input": {
+		"MAIN_TILE": "Snapshot Input",
+		"MAIN_INSTRUCTIONS_BASE": "Please answer the sections on the left, the sections on the right will give you instructions as well as best practices to answer each section",
+		"MAIN_INSTRUCTIONS_TIMING": "This should take a total of 15 minutes to complete – please do not spend anymore time on it. For the four sections you should spend about 2 to 3 minutes on each section and then review at the end for the remaining time to improve any answers. This a recommendation - just make sure to complete it in 15 minutes.",
+
+		"THIS_WEEK_TITLE": "This week I...",
+		"THIS_WEEK_INSTRUCTIONS": "Please summarize what you have worked on this week.",
+		"THIS_WEEK_FFT": [
+			"Try to keep it concise and use precise language.",
+			"What are the high level essential things you have accomplished (or failed)?"
+		],
+
+		"LEARNINGS_TITLE": "From this I learned...",
+		"LEARNINGS_PROBLEM_TITLE": "Problem",
+		"LEARNINGS_INTERVENTION_TITLE": "Intervention",
+		"LEARNINGS_RESULTS_TITLE": "Results",
+		"LEARNINGS_PROBLEM_INSTRUCTIONS": "Write your overall research question or problem you are trying to solve.",
+		"LEARNINGS_INTERVENTION_INSTRUCTIONS": "Write the design/intervention you are using to solve this problem.",
+		"LEARNINGS_RESULTS_INSTRUCTIONS": "Write the result of your work this week in terms of the intervention- could be designing, building, or testing the intervention.",
+		"LEARNINGS_PROBLEM_FFT": [
+			"What core obstacle or problem are you trying to solve?",
+			"What questions are you trying to answer with your design arguments?",
+			"What user struggles are you meeting?"
+		],
+		"LEARNING_INTERVENTION_FFT": [
+			"What design are you using to solve this problem?",
+			"What is your technique/technology you are using to solve the problem?",
+			"Why does it work?  Why might it fail?"
+		],
+		"LEARNING_RESULTS_FFT": [
+			"What is your deliverable?",
+			"What are your learnings of your work?"
+		],
+
+		"REFLECTION_TITLE": "I reflected on...",
+		"REFLECTION_ISSUE_TITLE": "Observed Issue",
+		"REFLECTION_IMPACT_TITLE": "Impact",
+		"REFLECTION_CAUSE_TITLE": "High-Level Cause",
+		"REFLECTION_ISSUE_INSTRUCTIONS": "Write any issue you have. Try to focus on bigger issues rather than smaller ones.",
+		"REFLECTION_IMPACT_INSTRUCTIONS": "Write down the impact of this issue – what happened as a result of it? What failed or did not succeed fully?",
+		"REFLECTION_CAUSE_INSTRUCTIONS": "Write down the high-level cause of the observed issue. What was the root cause of the issue?",
+		"REFLECTION_ISSUE_FFT": [
+			"What are the biggest bottlenecks to your project?",
+			"What are your personal meta-blockers that you would want to bring up?",
+		],
+		"REFLECTION_IMPACT_FFT": [
+			"Think about the result of your issues objectively - good and bad",
+			"Was the impact high, medium, or low? Does it matter?",
+		],
+		"REFLECTION_CAUSE_FFT": [
+			"Ask yourself “Why” until you are at the root of the issue.",
+			"At a high level, what led to your observed issue?",
+			"What contextual/teamwork/process failures led to your issue?"
+		],
+
+		"NEXT_STEPS_TITLE": "Moving forward, I will...",
+		"NEXT_STEPS_INSTRUCTIONS": "These are summaries for the  next week bulleted similar to the first section above. Look at your learnings and reflection section to answer this question. Be thoughtful please!",
+		"NEXT_STEPS_FFT": [
+			"What is useful for your project? How can you get the most learning for the least points?",
+			"Is the work you are doing actually addressing core user problems/problems in your domain? What is your justification.",
+			"Is your design argument working? How can update it to address the core obstacles better to meet your outcomes?"
+		],
+	}
 }
--- imports/ui/SnapshotInput.jsx
+++ imports/ui/SnapshotInput.jsx
@@ -2,6 +2,7 @@ import React, { Component, PropTypes } from 'react';
 import ReactDOM from 'react-dom';
 
 import { Grid, Row, Col } from 'react-flexbox-grid';
+import RichEditor from './RichEditor.jsx';
 
 import Strings from '../strings/strings.js';
 
@@ -11,13 +12,61 @@ class SnapshotInput extends Component {
       <Grid>
       	<Row>
       		<Col xs={12}>
-      			<h1>{Strings.SNAPSHOT_MAIN_TILE}</h1>
+      			<h1>{Strings.Input.MAIN_TILE}</h1>
       		</Col>
       	</Row>
       	<Row>
       		<Col xs={12}>
-      			<p>{Strings.SNAPSHOT_MAIN_INSTRUCTIONS_BASE}</p>
-      			<p>{Strings.SNAPSHOT_MAIN_INSTRUCTIONS_TIMING}</p>
+      			<p>{Strings.Input.MAIN_INSTRUCTIONS_BASE}</p>
+      			<p>{Strings.Input.MAIN_INSTRUCTIONS_TIMING}</p>
+      		</Col>
+      	</Row>
+      	<Row>
+      		<Col xs={12}>
+      			<h2>{Strings.Input.THIS_WEEK_TITLE}</h2>
+      			<p>{Strings.Input.THIS_WEEK_INSTRUCTIONS}</p>
+      			<RichEditor />
+      		</Col>
+      	</Row>
+      	<Row>
+      		<Col xs={12}>
+      			<h2>{Strings.Input.LEARNINGS_TITLE}</h2>
+
+      			<h4>{Strings.Input.LEARNINGS_PROBLEM_TITLE}</h4>
+      			<p>{Strings.Input.LEARNINGS_PROBLEM_INSTRUCTIONS}</p>
+      			<RichEditor />
+
+      			<h4>{Strings.Input.LEARNINGS_INTERVENTION_TITLE}</h4>
+      			<p>{Strings.Input.LEARNINGS_INTERVENTION_INSTRUCTIONS}</p>
+      			<RichEditor />
+
+      			<h4>{Strings.Input.LEARNINGS_RESULTS_TITLE}</h4>
+      			<p>{Strings.Input.LEARNINGS_RESULTS_INSTRUCTIONS}</p>
+      			<RichEditor />
+      		</Col>
+      	</Row>
+      	<Row>
+      		<Col xs={12}>
+      			<h2>{Strings.Input.REFLECTION_TITLE}</h2>
+
+      			<h4>{Strings.Input.REFLECTION_ISSUE_TITLE}</h4>
+      			<p>{Strings.Input.REFLECTION_ISSUE_INSTRUCTIONS}</p>
+      			<RichEditor />
+
+      			<h4>{Strings.Input.REFLECTION_IMPACT_TITLE}</h4>
+      			<p>{Strings.Input.REFLECTION_IMPACT_INSTRUCTIONS}</p>
+      			<RichEditor />
+
+      			<h4>{Strings.Input.REFLECTION_CAUSE_TITLE}</h4>
+      			<p>{Strings.Input.REFLECTION_CAUSE_INSTRUCTIONS}</p>
+      			<RichEditor />
+      		</Col>
+      	</Row>
+      	<Row>
+      		<Col xs={12}>
+      			<h2>{Strings.Input.NEXT_STEPS_TITLE}</h2>
+      			<p>{Strings.Input.NEXT_STEPS_INSTRUCTIONS}</p>
+      			<RichEditor />
       		</Col>
       	</Row>
       </Grid>
