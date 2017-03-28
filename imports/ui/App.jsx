4c2ad65ec5cf7e56d3800c6576962703470a55a6
Bomani McClendon <bomanimcclendon2017@u.northwestern.edu>
2017-03-28 11:02:36 -0500

    Sets up basic React in the meteor project via the App.jsx file

---
+++ imports/ui/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component, PropTypes } from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>APP</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
