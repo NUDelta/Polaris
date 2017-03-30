ad5342a0514e9d2931ec49e50f771d8f29d0c6be
Bomani McClendon <bomanimcclendon2017@u.northwestern.edu>
2017-03-30 16:41:23 -0500

    Testing using selector to update user profile

--- imports/ui/HomePage.jsx
+++ imports/ui/HomePage.jsx
@@ -1,17 +1,87 @@
 import React, { Component, PropTypes } from 'react';
 import ReactDOM from 'react-dom';
+import { Meteor } from 'meteor/meteor';
+import { createContainer } from 'meteor/react-meteor-data';
 
 import AccountsUIWrapper from './AccountsUIWrapper.jsx';
+import { Projects } from '../api/projects.js';
 
-class App extends Component {
+class HomePage extends Component {
+	constructor(props) {
+		super(props);
+
+		this.handleChange = this.handleChange.bind(this);
+	}
+
+	handleChange(event) {
+		if (event.target.value != 'none') {
+			Meteor.users.update(Meteor.userId(), {
+				$set: {project: event.target.value}
+			});
+		}
+  }
+
+  getUserProject() {
+  	let userProject = "none";
+
+  	if ('project' in Meteor.user()) {
+  		userProject = Meteor.user().project;
+  	}
+
+  	return userProject;
+  }
+
+	createProjectSelector() {
+		let options = []
+
+		options.push(
+			<option key={0} value="none">None</option>
+		);
+
+		this.props.projects.map(function(proj) {
+			options.push(
+				<option key={proj._id} value={proj._id}>{proj.name}</option>
+			);
+		});
+
+		return (
+			<select 
+				name="select" 
+				value={this.getUserProject()}
+				onChange={this.handleChange}>
+				{options}
+			</select>
+		);
+
+	}
+
   render() {
     return (
       <div className="container">
       	<AccountsUIWrapper />	
-        <h1>HomePage</h1>
+        <h1>Snapshot</h1>
+        <p>
+	        Lorem ipsum dolor sit amet, consectetur 
+	        adipiscing elit, sed do eiusmod tempor incididunt 
+	        labore et dolore magna aliqua. Ut enim ad minim 
+	        veniam, quis nostrud exercitation ullamco laboris 
+	        nisi ut aliquip ex ea commodo consequat.
+        </p>
+        {this.props.currentUser 
+        	? <div>
+        			<h5>Select Your Project</h5>
+        			{this.createProjectSelector()}
+        		</div>
+        	: <div></div>
+	      }
       </div>
     );
   }
 }
 
-export default App;
+export default createContainer(() => {
+  return {
+    projects: Projects.find().fetch(),
+    currentUser: Meteor.user(),
+  };
+}, HomePage);
