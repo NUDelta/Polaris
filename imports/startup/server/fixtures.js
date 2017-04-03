862e491620c10eaad94337af764f18125618ee76
Bomani McClendon <bomanimcclendon2017@u.northwestern.edu>
2017-04-03 13:10:01 -0500

    Adds fixtures for each current project

--- imports/startup/server/fixtures.js
+++ imports/startup/server/fixtures.js
@@ -6,11 +6,60 @@ class Fixtures {
 	static projectFixtures() {
 		Projects.update(
 			{
+				name: "Haoqi Zhang"
+			},
+			{
+				name: "Haoqi Zhang"
+			},
+			{upsert: true}
+		);
+
+		Projects.update(
+			{
+				name: "Pair Research"
+			},
+			{
+				name: "Pair Research"
+			},
+			{upsert: true}
+		);
+
+		Projects.update(
+			{
 				name: "Polaris"
 			},
 			{
-				name: "Polaris",
-				members: []
+				name: "Polaris"
+			},
+			{upsert: true}
+		);
+
+		Projects.update(
+			{
+				name: "Collective Experience Stories"
+			},
+			{
+				name: "Collective Experience Stories"
+			},
+			{upsert: true}
+		);
+
+		Projects.update(
+			{
+				name: "Frankenstein"
+			},
+			{
+				name: "Frankenstein"
+			},
+			{upsert: true}
+		);
+
+		Projects.update(
+			{
+				name: "MF-RPPT"
+			},
+			{
+				name: "MF-RPPT"
 			},
 			{upsert: true}
 		);
@@ -20,8 +69,57 @@ class Fixtures {
 				name: "LES"
 			},
 			{
-				name: "LES",
-				members: []
+				name: "LES"
+			},
+			{upsert: true}
+		);
+
+		Projects.update(
+			{
+				name: "Microreminders"
+			},
+			{
+				name: "Microreminders"
+			},
+			{upsert: true}
+		);
+
+		Projects.update(
+			{
+				name: "Brain Points"
+			},
+			{
+				name: "Brian Points"
+			},
+			{upsert: true}
+		);
+
+		Projects.update(
+			{
+				name: "Creative Code Competition"
+			},
+			{
+				name: "Creative Code Competition"
+			},
+			{upsert: true}
+		);
+
+		Projects.update(
+			{
+				name: "Perseus"
+			},
+			{
+				name: "Perseus"
+			},
+			{upsert: true}
+		);
+
+		Projects.update(
+			{
+				name: "Scaffolded Exercises"
+			},
+			{
+				name: "Scaffolded Exercises"
 			},
 			{upsert: true}
 		);
