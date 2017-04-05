ed1a8388afed1bcafa4a6b2b77e3a1252bd3c647
sameersrivastava <sameer.srivastava95@gmail.com>
2017-04-04 20:26:50 -0500

    looks like autosave is working and loading data from before.

--- imports/ui/MyEditor.jsx
+++ imports/ui/MyEditor.jsx
@@ -1,17 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Editor, EditorState, RichUtils, convertToRaw, convertFromRaw } from 'draft-js';
+import { EditorState, RichUtils, convertToRaw, convertFromRaw } from 'draft-js';
+
+import Editor from 'draft-js-plugins-editor';
+import createAutosavePlugin from 'draft-js-autosave-plugin';
+
+
 
 import { Snapshots } from '../api/snapshots.js';
 
 
+
+
 class MyEditor extends React.Component {
+  
+
   constructor(props) {
     super(props);
-    this.state = {editorState: EditorState.createEmpty()};
+    this.state = {
+    	editorState: this.createEditorState(),
+    	firstEdit: false
+    };
+
+    const config = {
+      saveFunction: this.saveToDB.bind(this),
+      debounceTime: 500,
+    	saveAlways: true
+    }
+
+    this.autosavePlugin = createAutosavePlugin(config);
   }
 
   onChange(editorState){
+  	this.setState({editorState:editorState});
+  	/*
     if (this.props.snapshotID) {
     	let setAction = {}
     	const contentState = editorState.getCurrentContent();
@@ -21,24 +43,36 @@ class MyEditor extends React.Component {
     	Snapshots.update(this.props.snapshotID, {
 	      $set: setAction,
 	    });
-    }
+    }*/
   }
 
   createEditorState(){
-  	if(this.props.snapshotID){
-  		if(this.props.text) {
+  	if(this.props.text) {
 
-  			const rawContent = this.props.text;
-  			const contentState = convertFromRaw(JSON.parse(rawContent));
-  			const editorStateFromDB = EditorState.moveFocusToEnd(EditorState.createWithContent(contentState));
-
-  			return editorStateFromDB;
-  		} else {
-  			return EditorState.createEmpty()
-  		}
-  	} else {
-  		return EditorState.createEmpty();
+		const rawContent = this.props.text;
+		const contentState = convertFromRaw(JSON.parse(rawContent));
+		const editorStateFromDB = EditorState.moveFocusToEnd(EditorState.createWithContent(contentState));
+		this.setState({firstEdit:true});
+		return editorStateFromDB;
+	} else {
+		this.setState({firstEdit:true});
+  		return EditorState.createEmpty()
   	}
+  	
+  }
+
+  saveToDB() {
+  	console.log('saveToDB');
+  	if (this.props.snapshotID) {
+    	let setAction = {}
+    	const contentState = this.state.editorState.getCurrentContent();
+    	const rawContent = JSON.stringify(convertToRaw(contentState));
+
+    	setAction[this.props.field] = rawContent;
+    	Snapshots.update(this.props.snapshotID, {
+	      $set: setAction,
+	    });
+    }
 
   }
 
@@ -50,12 +84,20 @@ class MyEditor extends React.Component {
       <div>
         <button onClick={this._onBoldClick.bind(this)}>Bold</button>
         <Editor
-          editorState={this.createEditorState()}
+          editorState={this.state.editorState}
           handleKeyCommand={this.handleKeyCommand}
           onChange={this.onChange.bind(this)}
+          plugins={[this.autosavePlugin]}
         />
       </div>
     );
+  }
+  componentDidUpdate(nextProps, nextState) {
+  	if(this.props.snapshotID) {
+  		if(!this.state.firstEdit) {
+  			this.setState({editorState:this.createEditorState()});
+  		}
+  	}
   }
 }
 
